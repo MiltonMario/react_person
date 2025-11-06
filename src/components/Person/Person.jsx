748fcb04c1825192014d3f3d2efd89bb1c2fdd8d
MiltonMario <miltonneto0010@gmail.com>
2025-11-06 11:47:41 -0300

    add solution #1

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,25 +1,28 @@
-function MarriedInfo({ person }) {
-  if (person.sex === 'm') {
+function MarriedInfo({ sex, partnerName }) {
+  if (sex === 'm') {
     return (
-      <p className="Person__partner">{`${person.partnerName} is my wife`}</p>
+      <p className="Person__partner">{`${partnerName} is my wife`}</p>
+    );
+  } else {
+    return (
+      <p className="Person__partner">{`${partnerName} is my husband`}</p>
     );
   }
-
-  return (
-    <p className="Person__partner">{`${person.partnerName} is my husband`}</p>
-  );
 }
 
-export const Person = ({ person }) => (
-  <section className="Person">
-    <h2 className="Person__name">{`My name is ${person.name}`}</h2>
-    {person.age != null && (
-      <p className="Person__age">{`I am ${person.age}`}</p>
-    )}
-    {person.isMarried ? (
-      <MarriedInfo person={person} />
-    ) : (
-      <p className="Person__partner">I am not married</p>
-    )}
-  </section>
-);
+export const Person = ({ person }) => {
+  const { name, age, isMarried, sex, partnerName } = person;
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${name}`}</h2>
+      {age != null && <p className="Person__age">{`I am ${age}`}</p>}
+      {isMarried ? (
+        <MarriedInfo sex={sex} partnerName={partnerName} />
+      ) : (
+        <p className="Person__partner">I am not married</p>
+      )}
+    </section>
+  );
+};
+
